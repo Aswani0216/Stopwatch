34c47f0f15cf8fbe75c16feaf617aaf41e56a79f
Aswini_Pappusetty <pappusettyaswini@gmail.com>
2025-07-24 10:17:44 +0530

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import datetime
+import time
+
+class Timer(object):
+    """A simple timer class"""
+    def start(self):
+        self.start_time = datetime.datetime.now()
+        print("Stopwatch started.")
+    def stop(self):
+        self.end_time = datetime.datetime.now()
+        print("Stopwatch stopped.")
+        print("Elapsed:", self.end_time - self.start_time)
+
+timer = Timer()
+timer.start()
+input("Press Enter to stop the stopwatch...")
+timer.stop()
+
+
+# In[ ]:
+
+
+
+
